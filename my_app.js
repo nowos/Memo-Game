9e0dd2526a80566d167248eed61b7e161c9e30ca
m.nowosielski <michal.nowosielski.90@gmail.com>
2017-11-30 15:56:23 +0100

    second

--- my_app.js
+++ my_app.js
@@ -3,8 +3,10 @@ var two = 'empty';
 var click = 0;
 var round = 0;
 var matched = 0;
-var cards = 0;
+var cards;
 var l = 0;
+var level;
+var imagesBox = new Array;
 
 var imagesBox1 = [
    "images/WilkMEM.jpg",
@@ -14,10 +16,8 @@ var imagesBox1 = [
    "images/LionMEM.jpg",
    "images/WilkMEM.jpg",
    "images/owcaMEM.jpg",
-   "images/LionMEM.jpg",
    "images/KonMEM.jpg"
 ];
-
 var imagesBox2 = [
    "images/ChichenMEM.jpg",
    "images/ColosseumMEM.jpg",
@@ -26,18 +26,16 @@ var imagesBox2 = [
    "images/TajMahalMEM.jpg",
    "images/ColosseumMEM.jpg",
    "images/pyramidMEM.jpg",
-   "images/statueMEM.jpg",
+   "images/statueMEM.jpeg",
    "images/pyramidMEM.jpg",
    "images/TajMahalMEM.jpg",
    "images/EifelMEM.jpg",
-   "images/statueMEM.jpg"
+   "images/statueMEM.jpeg"
 ];
-
 var imagesBox3 = [
    "images/znak1.png",
    "images/znak2.png",
    "images/znak9.png",
-   "images/znak11.png",
    "images/znak5.png",
    "images/znak3.png",
    "images/znak6.png",
@@ -49,25 +47,20 @@ var imagesBox3 = [
    "images/znak7.png",
    "images/znak9.png",
    "images/znak2.png",
-   "images/znak10.png",
    "images/znak7.png",
-   "images/znak11.png",
-   "images/znak3.png",
-   "images/znak10.png"
-];
+   "images/znak3.png"
 
+];
 //shuffle function
 
 function shuffle(array) {
    var currentIndex = array.length, temporaryValue, randomIndex;
-
    // While there remain elements to shuffle...
    while (0 !== currentIndex) {
 
       // Pick a remaining element...
       randomIndex = Math.floor(Math.random() * currentIndex);
       currentIndex -= 1;
-
       // And swap it with the current element.
       temporaryValue = array[currentIndex];
       array[currentIndex] = array[randomIndex];
@@ -77,53 +70,43 @@ function shuffle(array) {
    return array;
 };
 
-//przycisk start
-$('.btStart').on('click', function () {
-   
-   imagesBox1 = shuffle(imagesBox1);
-   //wyzeruj rundy
-   round = 0;
-   //ustaw liczbę kart
-   cards = 9;
-   //ustaw level
-   l = 1;
-   
-});
+var buttons = function () {
 
 //przycisk start
-$('.btStart2').on('click', function () {
+   $('.btStart').on('click', function () {
 
-   imagesBox2 = shuffle(imagesBox2);
-   //wyzeruj rundy
-   round = 0;
-   //ustaw liczbę kart
-   cards = 12;
-   //ustaw level
-   l = 2;
+      imagesBox1 = shuffle(imagesBox1);
+      //wyzeruj rundy
+      round = 0;
 
-});
-
+   });
 //przycisk start
-$('.btStart3').on('click', function () {
+   $('.btStart2').on('click', function () {
 
-   imagesBox3 = shuffle(imagesBox3);
-   //wyzeruj rundy
-   round = 0;
-   //ustaw liczbę kart
-   cards = 20;
-   //ustaw level
-   l = 3;
+      imagesBox2 = shuffle(imagesBox2);
+      //wyzeruj rundy
+      round = 0;
+      //ustaw liczbę kart
+   });
+//przycisk start
+   $('.btStart3').on('click', function () {
 
-});
+      imagesBox3 = shuffle(imagesBox3);
+      //wyzeruj rundy
+      round = 0;
+      //ustaw liczbę kart
+
+   });
+};
 
 
 var removeImages = function () {
-   for (i = 1; i <= cards; i++) {
+
+   for (i = 1; i <= cards ; i++) {
       //usuń content, a potem samą klasę additional ze wszystkich div-ów, w których jest 
       $('.additional' + i).css({'content': ''});
       $('.d' + i)[0].classList.remove("additional" + i);
-   }
-   ;
+   };
 };
 var clickCounter = function () {
    if (click >= 2) {
@@ -131,30 +114,50 @@ var clickCounter = function () {
       click = 0;
    }
 };
-
 var roundFunction = function () {
    $('.round').text("Round " + round);
 };
-
 var winFunction = function () {
    alert('You won!! Congratulation!!');
    matched = 0;
 };
 
+var setLevel = function () {
+   level = $('.lvl').text();
+   console.log(level);
+   switch (level) {
+      case '1':
+         cards = 8;
+         imagesBox = imagesBox1;
+         break;
+      case '2':
+         cards = 12;
+         imagesBox = imagesBox2;
+         break;
+      case '3':
+         cards = 16;
+         imagesBox = imagesBox3;
+         break;
+   }
+   ;
+};
+
 //show image
 var clickFunction = function () {
-
+   console.log(cards);
+   console.log(imagesBox);
+   console.log(level);
 //pętla na divach
-
-   for (i = 0; i <= cards; i++) {
+//ustawienia różnych leveli
+   for (i = 1; i <= cards ; i++) {
       $('.d' + i).on('click', onClickFunction = function () {
 
-         //wyzeruj obrazki jeśli było ponad 2 cliknięcia
+
+  //wyzeruj obrazki jeśli było ponad 2 cliknięcia
          clickCounter();
          click = click + 1;
-
 //szukanie obrazka do kliknietego pola
-         for (var i = 0; i <= cards; i++) {
+         for (var i = 0; i <= cards ; i++) {
 
 //jeśli znaleziony będzie obrazek do pola, to:
             if ($(this)[0].className === "emptyImg d" + (i + 1)) {
@@ -165,17 +168,15 @@ var clickFunction = function () {
                char = imgChosen.charAt(imgChosen.length - 1);
 //dodanie klasy 
                $(this).addClass('additional' + char);
-
 //dodanie do dodanej klasy obrazka odpowiadającego kolejności div 
-               $('.additional' + char).css({'content': 'url' + "('" + imagesBox1[i] + "')"});
+               $('.additional' + char).css({'content': 'url' + "('" + imagesBox[i] + "')"});
                //jeśli one puste to przypisz nazwę obrazka
                if (one === 'empty') {
-                  one = imagesBox1[i];
-
+                  one = imagesBox[i];
                }
 //jeśli one pełne to przypisz nazwę obrazka do two
                else if (one !== 'empty') {
-                  two = imagesBox1[i];
+                  two = imagesBox[i];
                }
                //MATCHING
 
@@ -184,21 +185,21 @@ var clickFunction = function () {
                   //aktualizacja rundy
                   round = round + 1;
                   roundFunction();
-
                   if (one === two) {
                      one = 'empty';
                      two = 'empty';
                      matched = matched + 1;
-                     if (matched === 5) {
+                     var pairs = cards / 2;
+                     if (matched === pairs) {
                         winFunction();
                      }
 
 //jeśli będzie dopasowanie, to zainstaluj CT obrazek
-                     for (i = 1; i <= cards; i++) {
+                     for (i = 1; i <= cards ; i++) {
                         if ($('.d' + i).hasClass('additional' + i)) {
                            $('.d' + i).addClass('matched');
                         }
-                    ;
+                        ;
                      }
 //jeśli są niedopasowane
                   } else {
@@ -210,12 +211,13 @@ var clickFunction = function () {
             ;
          }
          ;
-      });//end of onClickFunction
+      }); //end of onClickFunction
    }
    ;
 };
-
 var main = function () {
+   setLevel();
+   buttons();
    clickFunction();
 };
 
